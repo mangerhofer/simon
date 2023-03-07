24d2a70eed75bc383caa6370d761af0fe00ebb04
McCall Angerhofer <mccallangerhofer@gmail.com>
2023-03-06 23:13:11 -0700

    fixing js code

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() {
-    const nameEL = document.querySelector('#name');
-    localStorage.setItem('userName', nameEl.value);
-    window.location.href = 'play.html';
-}
+    const nameEl = document.querySelector("#name");
+    localStorage.setItem("userName", nameEl.value);
+    window.location.href = "play.html";
+  }
